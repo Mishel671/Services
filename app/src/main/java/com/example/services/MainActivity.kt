5ae30bc541e89220ac629eea41fb53873a0487dc
Mishel617 <Dzuba18071990@gmail.com>
2021-10-12 16:40:32 +0500

    Services

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.services
 
+
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,11 +14,37 @@ class MainActivity : AppCompatActivity() {
     val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.simpleService.setOnClickListener {
-            startService(MyService.newIntent(this))
+            stopService(MyForegroundService.newIntent(this))
+            startService(MyService.newIntent(this, 25))
+        }
+        binding.foregroundService.setOnClickListener {
+            ContextCompat.startForegroundService(
+                this,
+                MyForegroundService.newIntent(this)
+            )
+        }
+        binding.intentService.setOnClickListener {
+            ContextCompat.startForegroundService(
+                this,
+                MyIntentService.newIntent(this)
+            )
+        }
+        binding.jobScheduler.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .build()
+
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+            jobScheduler.schedule(jobInfo)
         }
     }
 }
---
+++ app/src/main/java/com/example/services/MyJobService.kt
@@ -0,0 +1,58 @@
+package com.example.services
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.app.job.JobParameters
+import android.app.job.JobScheduler
+import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.*
+
+
+class MyJobService : JobService() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("onStartJob")
+        coroutineScope.launch {
+            for (i in 0 until 3) {
+                delay(1000)
+                log("Timer $i")
+            }
+            jobFinished(params, true)
+        }
+        return true
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        log("onDestroy")
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyJobService: $message")
+    }
+
+    companion object{
+        const val JOB_ID = 1
+    }
+}
